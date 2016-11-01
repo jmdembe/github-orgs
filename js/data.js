ae3ba3de10a5858e3ab72c3c96ec65af54edd579
Jessica Dembe <jmdembe@gmail.com>
2016-11-01 15:32:11 -0400

    Connected AJAX script and created AJAX call for github info

--- js/data.js
+++ js/data.js
@@ -1,4 +1,25 @@
 (function() {
     'use strict';
 
+
+    $('button')
+        .on('click', function getGHInfo(event) {
+            var token = $('input').val();
+            $.ajax({
+                url: 'https://api.github.com/users/jisaacks/orgs',
+                dataType: 'json',
+                headers: {
+                    Authorization: 'token ' + token
+                }
+            })
+        .done(function handleGHOrgs(data){
+            console.log("YAY DATA!");
+            console.log(data);
+        })
+        .fail(function handleGHOrgError(xhr) {
+            console.log("Well this is embarrassing...", xhr);
+        });
+
+      });
+
 })();
